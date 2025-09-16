4046460d71403e099bfb7bbf648e2c2d7a0abbcf
Eliseu <eliseu.nfa@gmail.com>
2025-09-16 16:36:10 -0300

    Criação da pagina de login da aplicação planivo e testes com js para verificação de valores do input

---
+++ planivo.js
@@ -0,0 +1,10 @@
+//função do botao de prosseguir da area login
+function prosseguirLogin(){
+    let nomeUser = document.getElementById('nomeUser').value;
+    
+    if(nomeUser == '' || nomeUser == ' '){
+        alert('Preencha corretamente o campo nome para prosseguir!');
+    }else{
+        console.log(nomeUser);
+    }
+}
