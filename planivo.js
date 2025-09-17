2f424cc68e6d2adbcd5e1936411b195cd9384882
Eliseu <eliseu.nfa@gmail.com>
2025-09-17 09:50:51 -0300

    Criado pasta para guardar os html dos dias da semana , criado layout faltando apenas o footer da segunda.html que será o primeiro dia jogado para o usuario poder acessar os outros após, e adicionado um location para a segunda.html apos clicar no prosseguir presente no index.html

--- planivo.js
+++ planivo.js
@@ -6,5 +6,6 @@ function prosseguirLogin(){
         alert('Preencha corretamente o campo nome para prosseguir!');
     }else{
         console.log(nomeUser);
+        window.location.href = "diashtml/segunda.html";
     }
 }
