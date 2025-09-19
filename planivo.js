f8ad9458db88d244b48b4c7bfa567a3a7c2d3bb3
Eliseu <eliseu.nfa@gmail.com>
2025-09-19 10:38:18 -0300

    ajustei btn de ebrir areas para renomear e adicionar tarefa e icones de fechar areas

--- planivo.js
+++ planivo.js
@@ -1,6 +1,9 @@
 let areaAddTask = document.getElementById("areaAddTask");
 areaAddTask.style.display = "none";
 
+let areaRenomear = document.getElementById("areaRenomear");
+areaRenomear.style.display = 'none';
+
 
 //função do botao de prosseguir da area login
 function prosseguirLogin(){
@@ -14,12 +17,19 @@ function prosseguirLogin(){
     }
 }
 
+function renomear(){
+    areaRenomear.style.display = 'flex';
+}
+
 //função para o btn que adiciona tarefa
 function novaTarefa(){
     areaAddTask.style.display = "flex";
-
 }
 
-function fecharArea(){
+function fecharArea(Area){
+    if(Area == 1){
     areaAddTask.style.display = "none";
+    }else{
+    areaRenomear.style.display = 'none';
+    }
 }
